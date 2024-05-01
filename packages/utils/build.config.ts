786ebfbd15e6e73a8c9666b1534d5390caf20150
kai <kaienyeo225@gmail.com>
2024-05-01 17:46:15 +0800

    chore: use iife build conf

--- packages/utils/build.config.ts
+++ packages/utils/build.config.ts
@@ -1,13 +1,23 @@
+import { renameSync } from 'node:fs'
+import { resolve } from 'node:path'
 import { defineBuildConfig } from 'unbuild'
 
 export default defineBuildConfig({
   entries: [
     'src/index',
-    'src/userscript'
   ],
-  declaration: true,
   clean: true,
+  declaration: true,
   rollup: {
     emitCJS: true,
+    output: {
+      name: 'utils',
+      format: 'iife',
+    },
+  },
+  hooks: {
+    'build:done': function ({ options }) {
+      renameSync(resolve(options.outDir, 'index.mjs'), resolve(options.outDir, 'index.js'))
+    },
   },
 })
