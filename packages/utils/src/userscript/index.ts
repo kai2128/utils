4275693c2392670cd8a214a2dfe7c908663f966f
kai <kaienyeo225@gmail.com>
2024-05-01 16:20:46 +0800

    feat: userscript

--- packages/utils/src/userscript/index.ts
+++ packages/utils/src/userscript/index.ts
@@ -1,3 +1,131 @@
-export function test() {
-    return 'test'
+export function waitForSecond(ms: number) {
+import { waitForSecond } from './index';
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms)
+  })
+}
+
+export function waitUntil(condition: () => boolean, action: (() => void) | undefined = undefined, timeout = -1) {
+  return new Promise((resolve, reject) => {
+    if (condition())
+      return resolve(true)
+
+    const interval = setInterval(() => {
+      if (action)
+        action()
+      if (condition()) {
+        clearInterval(interval)
+        resolve(true)
+      }
+    }, 1000)
+
+    if (timeout > 0) {
+      setTimeout(() => {
+        clearInterval(interval)
+        reject(new Error('Timeout'))
+      }, timeout)
+    }
+  })
+}
+
+export function waitForElms(selector: string) {
+  return new Promise((resolve) => {
+    if (document.querySelectorAll(selector))
+      return resolve(Array.from(document.querySelectorAll(selector)))
+
+    const observer = new MutationObserver(() => {
+      if (document.querySelectorAll(selector)) {
+        observer.disconnect()
+        resolve(Array.from(document.querySelectorAll(selector)))
+      }
+    })
+
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+    })
+  })
+}
+
+export function waitForElm(selector: string, timeout = 0): Promise<Element | null> {
+  return new Promise((resolve, reject) => {
+    if (timeout > 0) {
+      setTimeout(() => {
+        // eslint-disable-next-line prefer-promise-reject-errors
+        reject(null)
+      }, timeout)
+    }
+
+    if (document.querySelector(selector))
+      return resolve(document.querySelector(selector))
+
+    const observer = new MutationObserver(() => {
+      if (document.querySelector(selector)) {
+        observer.disconnect()
+        resolve(document.querySelector(selector))
+      }
+    })
+
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+    })
+  })
+}
+
+export async function waitForPageReady(bufferTime = 3000, timeout = -1) {
+  try {
+    await waitUntil(() => document.readyState === 'complete', undefined, timeout)
+    await waitForSecond(bufferTime) // wait for some more time to make sure the page is fully loaded
+  }
+  catch (_) {
+    // eslint-disable-next-line no-console
+    console.log(`timeout waiting element, refreshing page...`)
+    setTimeout(() => window.location.reload(), 1000)
+  }
+}
+
+export async function getInnerText(selector: string) {
+  try {
+    const elm = await waitForElm(selector, 3000)
+    return elm ? elm.textContent : ''
+  }
+  catch (e) {
+    console.error(selector, e)
+    return ''
+  }
+}
+
+export function triggerMouseEvent(node: Node, eventType = 'click') {
+  const clickEvent = new MouseEvent(eventType, {
+    bubbles: true,
+    cancelable: false,
+  });
+  node.dispatchEvent(clickEvent)
+}
+
+export function setStatusText(...text: unknown[]) {
+  text = text.map((t) => {
+    if (typeof t === 'object')
+      return JSON.stringify(t)
+
+    return t
+  })
+  const status = document.querySelector<HTMLDivElement>('#status')
+  if (status) {
+    status.textContent = text.join(' ')
+  }
+  else {
+    const status = document.createElement('div')
+    const statusText = document.createElement('pre')
+    status.style.position = 'fixed'
+    status.style.top = '0'
+    status.style.right = '0'
+    status.style.zIndex = '9999'
+    status.style.color = 'red'
+    status.id = 'status'
+    statusText.textContent = text.join(' ')
+    status.appendChild(statusText)
+    document.body.appendChild(status)
+  }
 }
